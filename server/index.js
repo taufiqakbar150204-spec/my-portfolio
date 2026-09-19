5f62f50c71eb4ebb98b273338241c900af636160
Muhamad Taufiq Akbar <taufiqakbar150204@gmail.com>
2026-09-19 19:32:36 +0700

    Allow all origins in CORS

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Security & Parsing Middlewares
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
